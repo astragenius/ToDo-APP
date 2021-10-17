75b1b0bd8c81f080f9e6312cc96244d98c8bb2ec
astragenius <darthmalak86@gmail.com>
2021-10-17 11:36:46 +0200

    finished 'clear complete' button functionallity

--- src/index.js
+++ src/index.js
@@ -15,72 +15,44 @@ import {dragDrop} from "./drag-and-drop";
 
 
 
+const listItems = document.querySelectorAll('.container_label > input');
+const content = document.querySelectorAll('.toDolist__content');
+const del = document.querySelectorAll('.delete__cross');
+const del_btn = document.getElementById('delete__btn');
 
-/* let dragSrcEl = null;
-
-function handleDragStart(e) {
-   
-    dragSrcEl = this;
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/html', this.innerHTML);
-}
-
-function handleDragEnd(e) {
-
-    list.forEach(function(el) {
-
-        el.classList.remove('hover');
-    });
-   
-};
-
-function handleDragOver(e) {
-
-    if(e.preventDefault) {
-        e.preventDefault();
-    }
-
-    return false;
-};
-
-function handleDragEnter(e) {
-
-    this.classList.add('hover');
-};
-
-function handleDragLeave(e) {
-
-    this.classList.remove('hover');
-}
-
-function handleDrop(e) {
-   if (e.stopPropagation) {
-       e.stopPropagation();
-   }
-    if(dragSrcEl !== this) {
-        dragSrcEl.innerHTML = this.innerHTML;
-        this.innerHTML = e.dataTransfer.getData('text/html');
-    }
-    return false;
-}
+console.log(content.childNodes);
 
 
 
 
-const list = document.querySelectorAll('.toDolist__content');
 
-list.forEach(item => {
+del.forEach(el => {
 
-    item.addEventListener('dragstart', handleDragStart);
-    item.addEventListener('dragover', handleDragOver);
-    item.addEventListener('dragenter', handleDragEnter);
-    item.addEventListener('dragleave', handleDragLeave);
-    item.addEventListener('dragend', handleDragEnd);
-    item.addEventListener('drop', handleDrop);
-}) */
+    el.addEventListener('click', function() {
 
 
+        this.parentElement.remove();
+        
 
+       
+    })
+})
+
+del_btn.addEventListener('click', function() {
+
+    listItems.forEach(el => {
+
+        if(el.disabled === false && el.checked === true) {
+
+          
+            el.parentElement.parentElement.parentElement.remove()
+            
+        }else {
+            return
+        }
+
+    })
+})
 
 
 
